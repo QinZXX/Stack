b27a2cf153ce78fc30b4b2337f4da4b3f97f1b58
QinZhixiang <qinzhixiang@qq.com>
2020-07-23 23:10:04 +0800

    2020/7/23

--- main.cpp
+++ main.cpp
@@ -3,5 +3,16 @@
 
 int main() {
     std::cout << "Stack Definition!" << std::endl;
+    Stack<int> a;
+    for(int i=0;i<10;i++)
+        a.push(i);
+    a.traverse([](int &a){std::cout<<a<<"->";});
+
+    for(int i=0;i<10;i++){
+        std::cout<<a.pop()<<"->";
+    }
+
+    a.traverse([](int &a){std::cout<<a<<"->";});
+
     return 0;
 }
