e67afab6b5bf8794a3ae2854a29e0a745486bc99
QinZhixiang <qinzhixiang@qq.com>
2020-07-29 18:01:55 +0800

    iteration version

--- main.cpp
+++ main.cpp
@@ -7,9 +7,12 @@ using namespace std;
 void convert(Stack<char>& S,__int64 n,int base){
     static char digit[] //0<n,1<base<=16,新进制下的数位符号，可视base的取值范围适当扩充
     ={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-    if( 0<n ){ // 余数不为0
-        S.push(digit[n%base]);//输出低位
-        convert(S,n/base,base); // 递归得到更高位，再逆序输出
+    __int64 reminder=n;
+    int temp; // 余数
+    while( 0 < reminder ){ // 商不为0
+        temp = reminder % base;
+        S.push(digit[temp]);//输出低位
+        reminder =reminder / base ;
     }
 }
 
